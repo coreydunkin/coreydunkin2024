d4cfa3c18527f99a1a627cd8a6600dcb72fe867e
coreydunkin <corey.dunkin@gmail.com>
2023-11-24 03:31:44 +1100

    implemented react three fiber, added colors

--- frontend/src/components/Background/Background.tsx
+++ frontend/src/components/Background/Background.tsx
@@ -1,27 +1,47 @@
 'use client';
 import React, {useEffect, useRef} from "react";
 import s from "./Background.module.scss";
-import Particles from "@/components/Background/Particles/Particles";
 
 const Background = () => {
   const animationRef = useRef(null);
   const scrollYRef = useRef(0);
   const timeRef = useRef(Math.random() * 10000);
+  const elRef = useRef(null);
 
   useEffect(() => {
+
+    // Initializations for will-change
+    elRef.current.style.willChange = '--posX, --posY, --rotate';
+
     const handleScroll = () => {
-      scrollYRef.current = window.scrollY;
+      const newScrollY = window.scrollY;
+
+      // Skip this frame if the scroll position did not change significantly
+      if(Math.abs(scrollYRef.current - newScrollY) < 5) return;
+
+      scrollYRef.current = newScrollY;
     };
 
     window.addEventListener('scroll', handleScroll);
 
-    const animate = (time) => {
+    const animate = () => {
       animationRef.current = requestAnimationFrame(animate);
-      const el = document.getElementById("background");
-      timeRef.current += 0.005; // This will make it more unpredictable
-      el.style.setProperty('--posX', ((Math.sin(timeRef.current) + 1) / 2) * 500);
-      el.style.setProperty('--posY', ((Math.cos(timeRef.current) + 1) / 2) * 500 + scrollYRef.current / 10);
-      el.style.setProperty('--rotate', ((Math.sin(timeRef.current) + 1) / 2) * 1000);
+      timeRef.current += 0.005;
+
+      const newPosX = ((Math.sin(timeRef.current) + 1) / 2) * 500;
+      const newPosY = ((Math.cos(timeRef.current) + 1) / 2) * 500 + scrollYRef.current / 10;
+      const newRotate = ((Math.sin(timeRef.current) + 1) / 2) * 1000;
+
+      // Skip this frame if the new values did not significantly change
+      if(
+        Math.abs(elRef.current.style.getPropertyValue('--posX') - newPosX) < 5 &&
+        Math.abs(elRef.current.style.getPropertyValue('--posY') - newPosY) < 5 &&
+        Math.abs(elRef.current.style.getPropertyValue('--rotate') - newRotate) < 5
+      ) return;
+
+      elRef.current.style.setProperty('--posX', newPosX);
+      elRef.current.style.setProperty('--posY', newPosY);
+      elRef.current.style.setProperty('--rotate', newRotate);
     };
 
     animationRef.current = requestAnimationFrame(animate);
@@ -29,12 +49,16 @@ const Background = () => {
     return () => {
       window.removeEventListener('scroll', handleScroll);
       cancelAnimationFrame(animationRef.current);
+
+      // Clean up will-change
+      elRef.current.style.willChange = 'auto';
     };
   }, []);
+
+  // Added reference to the element
   return (
     <>
-      <Particles />
-      <section id="background" className={s.background} />
+      <section ref={elRef} id="background" className={s.background} />
     </>
   );
 };
