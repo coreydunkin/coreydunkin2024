4b75795ff6dbdca070f5b06a113035b90ee4fba6
coreydunkin <corey.dunkin@gmail.com>
2023-11-27 01:24:34 +1100

    clean up, introduce prettier, linting

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,11 @@
 import BackgroundCanvas from "@/components/BackgroundCanvas/BackgroundCanvas";
 import Container from "@/components/Container/Container";
 import Intro from "@/components/Intro/Intro";
+import MenuList from "@/components/MenuList/MenuList";
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between">
+      <MenuList />
       <Container>
         <Intro />
       </Container>
