cc4ab278f2d76f6efe726395617164bb82f91369
coreydunkin <corey.dunkin@gmail.com>
2023-12-03 00:37:33 +1100

    changed links to pull from future dynamic data

--- frontend/src/components/PortfolioNav/PortfolioNav.tsx
+++ frontend/src/components/PortfolioNav/PortfolioNav.tsx
@@ -1,6 +1,7 @@
 "use client";
-import Link from "next/link";
+import PortfolioItem from "@/components/PortfolioNav/PortfolioItem";
 import {usePathname} from "next/navigation";
+import {PORTFOLIO_LINKS} from "@/utils/constants";
 // build the nav out of the routes
 export default function PortfolioNav() {
   const pathname = usePathname();
@@ -9,24 +10,13 @@ export default function PortfolioNav() {
   }
   return (
     <div className="fixed z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
-      <Link href="/portfolio/test">
-        <button
-          type="button"
-          className={`w-3 h-3 ${pathname === "/portfolio/test" ? "opacity-100" : "opacity-30"} rounded-full bg-white`}
-        ></button>
-      </Link>
-      <Link href="/portfolio/test2">
-        <button
-          type="button"
-          className={`w-3 h-3 ${pathname === "/portfolio/test2" ? "opacity-100" : "opacity-30"} rounded-full bg-white`}
-        ></button>
-      </Link>
-      <Link href="/portfolio/test3">
-        <button
-          type="button"
-          className={`w-3 h-3 ${pathname === "/portfolio/test3" ? "opacity-100" : "opacity-30"} rounded-full bg-white`}
-        ></button>
-      </Link>
+      {PORTFOLIO_LINKS.map((portfolioLink) => (
+        <PortfolioItem
+          key={portfolioLink.id}
+          link={portfolioLink}
+          pathname={pathname}
+        />
+      ))}
     </div>
   )
 }
---
+++ frontend/src/utils/constants.ts
@@ -0,0 +1,45 @@
+
+// fill in the MAIN_LINKS for me with an id and a name
+export const MAIN_LINKS = [
+  {
+    id: 1,
+    name: "Home",
+    href: "/"
+  },
+  {
+    id: 2,
+    name: "About",
+    href: "/about"
+  },
+  {
+    id: 3,
+    name: "Portfolio",
+    href: "/portfolio/test"
+  },
+  {
+    id: 4,
+    name: "Contact",
+    href: "/contact"
+  }
+]
+
+export const PORTFOLIO_LINKS = [
+  {
+    id: 3.1,
+    name: "Test",
+    href: "/portfolio/test"
+  },
+  {
+    id: 3.2,
+    name: "Test 2",
+    href: "/portfolio/test2"
+  },
+  {
+    id: 3.3,
+    name: "Test 3",
+    href: "/portfolio/test3"
+  }
+]
+
+
+
