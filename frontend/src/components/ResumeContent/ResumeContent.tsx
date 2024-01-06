9492a553eaa23ac8a8619d35695033e83a5c50b2
coreydunkin <corey.dunkin@gmail.com>
2024-01-06 19:53:15 +1100

    fixed resume styles

--- frontend/src/components/ResumeContent/ResumeContent.tsx
+++ frontend/src/components/ResumeContent/ResumeContent.tsx
@@ -1,11 +1,26 @@
 
 import {EntryField, ResumeFields} from "@/lib/contentful/utils";
 import Image from "next/image";
-import PortfolioContent from "@/components/PortfolioContent/PortfolioContent";
 
 type ResumeContentProps = {
   data: ResumeFields;
 };
+
+type RichTextProps = {
+  text: string;
+}
+
+const RichText = ({ text }: RichTextProps) => {
+  const textArray = text.split('\n').filter(paragraph => paragraph);
+  const paragraphs = textArray.map((text, index) => {
+    const htmlText = text.replace(/__(.*?)__/g, '<strong>$1</strong>');
+    return (
+      <p key={index} className={`text-slate-600 ${index < textArray.length - 1 ? 'mb-5' : ''}`} dangerouslySetInnerHTML={{ __html: htmlText }} />
+    );
+  });
+
+  return <>{paragraphs}</>;
+}
 
 const TechList: React.FC<{ items: string[] }> = ({ items }) => {
   return (
@@ -27,8 +42,8 @@ const TechList: React.FC<{ items: string[] }> = ({ items }) => {
 const Content = ({ title, subTitle, date, description }: any) => {
   return (
     <div className="flex flex-col gap-6 mb-6">
-      <div className="flex">
-        <div className="mr-8 max-w-[100px] w-full text-slate-600 font-semibold">
+      <div className="flex flex-col sm:flex-row">
+        <div className="md:mb-4 sm:mr-8 sm:mb-0 md:max-w-[100px] w-full text-slate-600 font-semibold">
           {date}
         </div>
         <div className="flex flex-col flex-1">
@@ -37,7 +52,9 @@ const Content = ({ title, subTitle, date, description }: any) => {
             {subTitle}
           </p>
           {description ? (
-            <div className="text-slate-700 mt-2" dangerouslySetInnerHTML={{__html: description}} />
+            <div className="text-slate-700 mt-2">
+              <RichText text={description} />
+            </div>
           ) : null}
         </div>
       </div>
@@ -57,6 +74,7 @@ const ResumeContent = ({ data }: ResumeContentProps | any) => {
     apis,
     tools,
     workHistory,
+    education,
     resumeContacts
   } = data;
   const avatarFile = avatar?.file?.url || "";
@@ -101,7 +119,7 @@ const ResumeContent = ({ data }: ResumeContentProps | any) => {
         </section>
         <section className="my-9 text-sm">
           <h3 className="mb-1 text-slate-700 font-semibold text-lg">About</h3>
-          <div className="text-slate-600" dangerouslySetInnerHTML={{__html: about}} />
+          <RichText text={about} />
         </section>
 
         <section className="my-9 text-sm">
@@ -121,6 +139,13 @@ const ResumeContent = ({ data }: ResumeContentProps | any) => {
         </section>
 
         <section className="my-9 text-sm">
+          <h3 className="mb-3 text-slate-700 font-semibold text-lg">Education</h3>
+          {education.map((content: EntryField, index: number) => {
+            return <Content {...content} key={index} />;
+          })}
+        </section>
+
+        <section className="my-9 text-sm">
           <h3 className="mb-6 text-slate-700 font-semibold text-lg">Contact</h3>
           <div className="flex flex-col gap-6">
             {resumeContacts.length > 0 && resumeContacts.map((contact: EntryField, index: number) => {
@@ -129,7 +154,7 @@ const ResumeContent = ({ data }: ResumeContentProps | any) => {
                   <div className="mr-8 max-w-[100px] w-full text-slate-500 font-semibold">
                     {contact.label}
                   </div>
-                  <div className="flex flex-col flex-1 text-slate-900">
+                  <div className="flex flex-col flex-1 text-slate-600">
                     <a
                       href={contact.linkUrl}
                       target="_blank"
@@ -143,10 +168,11 @@ const ResumeContent = ({ data }: ResumeContentProps | any) => {
                         viewBox="0 0 12 12"
                         fill="none"
                         xmlns="http://www.w3.org/2000/svg"
+                        className="print:hidden"
                       >
                         <path
                           d="M3.5 3C3.22386 3 3 3.22386 3 3.5C3 3.77614 3.22386 4 3.5 4V3ZM8.5 3.5H9C9 3.22386 8.77614 3 8.5 3V3.5ZM8 8.5C8 8.77614 8.22386 9 8.5 9C8.77614 9 9 8.77614 9 8.5H8ZM2.64645 8.64645C2.45118 8.84171 2.45118 9.15829 2.64645 9.35355C2.84171 9.54882 3.15829 9.54882 3.35355 9.35355L2.64645 8.64645ZM3.5 4H8.5V3H3.5V4ZM8 3.5V8.5H9V3.5H8ZM8.14645 3.14645L2.64645 8.64645L3.35355 9.35355L8.85355 3.85355L8.14645 3.14645Z"
-                          className="fill-current text-slate-900"
+                          className="fill-current text-slate-600"
                         ></path>
                       </svg>
                     </a>
